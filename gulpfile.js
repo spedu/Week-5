062a4272c164707b25a207c609e71bc30cee1b8e
JaZahn <jcleveng@fas.harvard.edu>
2015-09-28 00:36:27 -0400

    editing index to be something simple

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,25 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var connect = require('gulp-connect');
+//var connect = require('gulp-connect');
 
 gulp.task('lint', function(){
-  return gulp.src('./src/*.js')
+  return gulp.src('./js/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('concat', function(){
-  return gulp.src('./src/*.js')
+  return gulp.src('./js/app/*.js')
+    .pipe(gulp.src('./js/app.js'))
     .pipe(concat('app.js'))
-    .pipe(uglify())
+    //.pipe(uglify())
     .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('copy', function(){
-  return gulp.src('./src/*.js').pipe(gulp.dest('./dist'));
+  return gulp.src('./js/**/*.js').pipe(gulp.dest('./dist'));
 });
 
-gulp.task('connect', connect.server({
-    root: ['src'],
-    port: 9000,
-    livereload: true
-}));
 
-
-gulp.task('default', ['concat', 'lint']);
+gulp.task('default', ['concat']);
