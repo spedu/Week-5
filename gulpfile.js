9ffef6db64ad05d1d46bf114b088d9d40b025a6b
JaZahn <jcleveng@fas.harvard.edu>
2015-09-28 01:46:22 -0400

    moving main.js and fixing gulpfiles concat task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-//var connect = require('gulp-connect');
 
 gulp.task('lint', function(){
   return gulp.src('./js/**/*.js')
@@ -12,9 +11,8 @@ gulp.task('lint', function(){
 
 gulp.task('concat', function(){
   return gulp.src('./js/app/*.js')
-    .pipe(gulp.src('./js/app.js'))
     .pipe(concat('app.js'))
-    //.pipe(uglify())
+    .pipe(uglify())
     .pipe(gulp.dest('./dist'));
 });
 
